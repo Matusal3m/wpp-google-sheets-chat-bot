819541761186c583f437b6ce1c3a1758a32fafef
Matusal3m <matusalem.dev@gmail.com>
2025-11-04 21:18:00 -0300

    removed files that will be unused

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,7 @@
-import { ModeToggle } from "./components/mode-toggle";
 import { ThemeProvider } from "./components/theme-provider";
 import "./index.css";
 import { Header } from "./partials/header";
 import { Footer } from "./partials/footer";
-import { StudentsTable } from "./partials/students-table";
 
 export function App() {
   return (
@@ -11,9 +9,7 @@ export function App() {
       <div className="w-screen flex flex-col min-h-screen">
         <Header />
 
-        <main className="flex-1 flex flex-col gap-6 p-6 bg-gray-950">
-          <StudentsTable />
-        </main>
+        <main className="flex-1 flex flex-col gap-6 p-6 "></main>
 
         <Footer />
       </div>
